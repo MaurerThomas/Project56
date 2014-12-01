79789cb2446aad9d7146fd00feed2d3ad4c3aa6a
ArmindoMaurits <a.r.maurits@gmail.com>
2014-12-01 10:29:32 +0100

    website updated met processor en moederbord jquery en header index
    website updated met processor en moederbord jquery en header index

--- Website/js/javascript.js
+++ Website/js/javascript.js
@@ -5,6 +5,43 @@
 			$('#pcbuilder').removeClass();
 			$('#start').hide();
 		});
+		
+		$("#moederbordsocketmerk").change(function (event) {
+		var socketmerk = $('#moederbordsocketmerk').val();
+			if(socketmerk == "AMD") {
+				$('#group1').show().fadeIn(); 
+				$("#group2").hide().fadeOut();
+			} else if ((socketmerk == "Intel")){
+				$('#group2').show().fadeIn(); 
+				$("#group1").hide().fadeOut();
+			} else {
+				$("#group1").hide().fadeOut();
+				$("#group2").hide().fadeOut();
+			}
+		});
+		
+		$("#processormerk").change(function (event) {
+		var processormerk = $('#processormerk').val();
+		var processorsocket = $('#processorsocketintel').val();
+		
+			if(processormerk == "AMD") {
+				$('#group3').show().fadeIn(); 
+				$("#group4").hide().fadeOut();
+				$("#group5").hide().fadeOut();
+			} else if ((processormerk == "Intel")){
+				$('#group4').show().fadeIn(); 
+				$("#group3").hide().fadeOut();
+				
+				$('#group5').show().fadeIn();
+				
+			} else {
+				$("#group3").hide().fadeOut();
+				$("#group4").hide().fadeOut();
+				$("#group5").hide().fadeOut();
+			}
+		});
+	
 	});
 	$(document).tooltip({position: {my: 'left top', at: 'right top'}});
+	
 })(jQuery);
