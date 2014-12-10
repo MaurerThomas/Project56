56ddca6d9d73b545477b1a905bba302b453fd5f0
U-EE-PC\EE <malusluminis@hotmail.com>
2014-12-10 19:41:23 +0100

    maak selectie verbergbaar

--- Website/js/javascript.js
+++ Website/js/javascript.js
@@ -10,6 +10,20 @@
 		if($compatibility.WebSocket) {
 			$webSocket.init('145.24.222.119','8080','/search');
 		}
+		$('#pcbuilder-selection-header h2 a.btn').click(function($e) {
+			var $btn = $(this);
+			$e.preventDefault();
+			if($btn.hasClass('glyphicon-chevron-up')) {
+				$btn.removeClass('glyphicon-chevron-up');
+				$btn.addClass('glyphicon-chevron-down');
+				$btn.attr('title','Geef uw selectie weer');
+			} else {
+				$btn.removeClass('glyphicon-chevron-down');
+				$btn.addClass('glyphicon-chevron-up');
+				$btn.attr('title','Verberg uw selectie');
+			}
+			$('#pcbuilder-selection').animate({height: 'toggle'});
+		});
 	});
 	$(document).tooltip({position: {my: 'left top', at: 'right top'}});
 })(jQuery);
