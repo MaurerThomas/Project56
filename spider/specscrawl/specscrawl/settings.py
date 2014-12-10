e02a415f5bc9c7b723605e5a30362dfba06219ad
Wouter Naloop <Baas@hotmail.com>
2014-12-10 20:54:35 +0100

    Bijna

--- spider/specscrawl/specscrawl/settings.py
+++ spider/specscrawl/specscrawl/settings.py
@@ -12,6 +12,18 @@ BOT_NAME = 'specscrawl'
 
 SPIDER_MODULES = ['specscrawl.spiders']
 NEWSPIDER_MODULE = 'specscrawl.spiders'
+DOWNLOAD_DELAY = 1
+# MongoDB pipeline configuration
+ITEM_PIPELINES = [
+  'scrapy_mongodb.MongoDBPipeline',
+]
 
+MONGODB_URI = 'mongodb://localhost:27017'
+MONGODB_DATABASE = 'mydb'
+MONGODB_COLLECTION = 'mydb'
+# Data buffering
+MONGODB_BUFFER_DATA = 10
+# Timestamp
+MONGODB_ADD_TIMESTAMP = True
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'specscrawl (+http://www.yourdomain.com)'
+#USER_AGENT = 'specs crawler (+145.24.222.119)'
--- spider/specscrawl/specscrawl/spiders/alternate.py
+++ spider/specscrawl/specscrawl/spiders/alternate.py
@@ -5,30 +5,58 @@ from specscrawl.items import SpecscrawlItem
 import re
 
 class AlternateSpider(Spider):
+#	def __init__(self):
+#		self.conn = MySQLdb.connect(user='pcbuilder', passwd='project', db='pcbuilder', host='127.0.0.1', charset='utf8', use_unicode=True)
+#		self.cursor = self.conn.cursor()
+#		try:
+#			start_urls = self.cursor.execute("""SELECT url FROM prijs_verloop WHERE crawled = 0""")
+#			print start_urls
+#			self.conn.commit()
+#
+#		except MySQLdb.Error, e:
+#			print "Error %d: %s" % (e.args[0], e.args[1])
+#	
 	name = "alternate"
 	allowed_domains = ["alternate.nl"]
 	start_urls = (
 		'https://www.alternate.nl/Aerocool/Dead-Silence-Black-White-Edition-behuizing/html/product/1096426?tk=7&lk=9309',
 	)
+	
 
 	def parse(self, response):
 		item = SpecscrawlItem()
-		datalist = response.xpath('//*[starts-with(@class,"techData")]')
+		datalist = response.xpath('//*[@class="techData"]')
+		print datalist
 		title = datalist.xpath('//*[starts-with(@class,"breadCrumbs")]/span/a/span/text()').extract()
 		title = title[1]
 		name = datalist.xpath('//*[starts-with(@class,"productNameContainer")]/*[starts-with(@itemprop,"name")]/text()').extract()
 		brand = datalist.xpath('//*[starts-with(@class,"productNameContainer")]/*[starts-with(@itemprop,"brand")]/text()').extract()
 		item['specs'] ={}
 		item['specs'] = {"name":name,"brand":brand}
-		for data in datalist:
-			tempkeys = data.xpath('//*[starts-with(@class,"techDataCol1")]/text()').extract()
-			tempvalue = data.xpath('//*[starts-with(@class,"techDataCol2")]/text()').extract()
-			print tempvalue
-			if(tempkeys):
-				for i in range(len(tempkeys)):
-					try:
-						item['specs'].update({tempkeys[i]:"hoi"})
-					except IndexError:
-						print tempkeys[i] + " does not exist"
-		#item.append(itemarray)
+		tempkeys = datalist.xpath('//*[@class="techDataCol1"]/text()').extract()
+		print tempkeys
+		tempvalue = datalist.xpath('//*[@class="techDataCol2"]')
+		if(tempkeys):
+			for i in range(len(tempkeys)):
+				value = tempvalue[i].extract()
+				keys = re.findall('techDataSubCol techDataSubColDescription">(.*?)</td><td' ,value)
+				values = re.findall('<td class="techDataSubCol techDataSubColValue"><table cellpadding="0" cellspacing="0"><tbody><tr><td class="techDataSubCol techDataSubColValue">(.*?)</td></tr></tbody></table><div class="break"> <!-- break --> </div></td>',value)	
+				
+				result = "null"
+				if len(keys) > 0:
+					print "----------------------"
+					print keys 
+					print values
+					result = {}
+					for n in range(len(keys)):
+						try:
+							result.update({keys[n]:"hoi"})
+						except IndexError:
+							print "x"
+				if result == "null":
+					result = re.findall('<td class="techDataSubCol techDataSubColValue">(.*)</td></tr></table>', value)
+				
+				print tempkeys[i] 
+				item['specs'].update({tempkeys[i]:result})
+				
 		yield item
