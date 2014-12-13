c5015c3a7c84ce268d598f40beac2af7fe9b5bcd
Thomas Maurer <0876337@hr.nl>
2014-12-13 12:19:56 +0100

    handleQuery update

--- Java/src/com/resist/pcbuilder/SearchHandler.java
+++ Java/src/com/resist/pcbuilder/SearchHandler.java
@@ -7,6 +7,7 @@ import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.index.query.FilterBuilders;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.json.JSONArray;
@@ -33,10 +34,27 @@ public class SearchHandler {
 	}
 
     public JSONArray handleMethod(JSONObject json) {
+            String outputType = "";
+            String outputMerk = "";
 
         if (json.has("type") && json.has("merk")) {
             System.out.println("Multimatch query");
-            handleQuery(json.getString("type"), json);
+            String jsonText = json.toString();
+            JSONArray jsonarr = new JSONArray("[" + jsonText + "]");
+
+            for(int i = 0; i < jsonarr.length(); i++){
+               JSONObject jsonobj = jsonarr.getJSONObject(i);
+              outputType = jsonobj.getString("type");
+              outputMerk = jsonobj.getString("merk");
+
+            }
+            List<String> output = new ArrayList<>();
+            output.add(outputType);
+            output.add(outputMerk);
+
+            System.out.println(jsonarr);
+                return handleQuery(output, json);
+
         } else {
             System.out.println("matchQuery");
             if (json.has("type")) {
@@ -46,13 +64,12 @@ public class SearchHandler {
         return null;
     }
 
-
-	public JSONArray handleQuery(String text,JSONObject json) {
+	public JSONArray handleQuery(List<String> text,JSONObject json) {
         int lengteJson = json.length();
         List<String> fields = new ArrayList<>();
-
+        System.out.println(text);
         System.out.println(lengteJson);
-        System.out.println(json);
+       // System.out.println(json);
 
         //Moederbord
         if (json.has("type") && json.has("merk")){
@@ -63,12 +80,14 @@ public class SearchHandler {
 
         if (lengteJson > 2) {
 
+
                   SearchResponse response = client
                     .prepareSearch("zoeker")
                     .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
-                    .setQuery(
-                            QueryBuilders.multiMatchQuery(text,fields.toArray(new String[fields.size()]))).setFrom(0).setSize(60)
-                    .setExplain(true).execute().actionGet();
+
+                    .setQuery(QueryBuilders.multiMatchQuery(text, fields.toString())).setFrom(0).setSize(60)
+                            .setPostFilter(FilterBuilders.termsFilter(String.valueOf(fields),text))
+                            .setExplain(true).execute().actionGet();
 
             SearchHit[] results = response.getHits().getHits();
             JSONArray resultaten = new JSONArray();
@@ -84,7 +103,6 @@ public class SearchHandler {
         }
 
     public JSONArray handleComponentType(String type, JSONObject json){
-
 
         SearchResponse response = client
                 .prepareSearch("zoeker")
