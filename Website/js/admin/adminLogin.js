1bfd9abe38f850ea13eb8905e1a8c8a100d7295a
EE <malusluminis@hotmail.com>
2014-12-17 12:01:47 +0100

    laat een andere warning zien als er geen username of wachtwoord is ingevoerd

--- Website/js/admin/adminLogin.js
+++ Website/js/admin/adminLogin.js
@@ -1,7 +1,8 @@
 $(function() {
 	var $formButton = $('form input[type="submit"]'),
 	$compatibility = checkCompatibility($formButton),
-	$loginWarning = $('form p.hidden');
+	$wrongLogin = $('#wrongLogin'),
+	$noLogin = $('#noLogin');
 	window.location.hash = '';
 	if($compatibility.WebSocket) {
 		$webSocket.init('145.24.222.119','8081','/admin');
@@ -9,11 +10,7 @@ $(function() {
 			var $json = JSON.parse($msg.data);
 			$formButton.attr('disabled',false);
 			if(!$json.login) {
-				$loginWarning.removeClass('hidden').fadeIn({
-					complete: function() {
-						$loginWarning.fadeOut(2000);
-					}
-				});
+				flashWarning($wrongLogin);
 			} else {
 				$('a.navbar-brand').attr('href','#');
 				$webSocket.receive = htmlHandler;
@@ -21,13 +18,27 @@ $(function() {
 		};
 	}
 	$('form').submit(function($e) {
+		var $username = $('input[name="username"]').val(),
+		$password = $('input[name="password"]').val();
 		$e.preventDefault();
+		if($username === '' || $password === '') {
+			flashWarning($noLogin);
+			return;
+		}
 		$formButton.attr('disabled',true);
 		$webSocket.send({
 			login: {
-				username: $('input[name="username"]').val(),
-				password: $('input[name="password"]').val()
+				username: $username,
+				password: $password
 			}
 		});
 	});
+
+	function flashWarning($loginWarning) {
+		$loginWarning.removeClass('hidden').fadeIn({
+			complete: function() {
+				$loginWarning.fadeOut(2000);
+			}
+		});
+	}
 });
