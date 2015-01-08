35ba2447d06cad4525828c6aeec817bd2d0dfaaa
Thomas Maurer <0876337@hr.nl>
2015-01-08 13:09:38 +0100

    Dashboard Grafieken + Google analytics toegevoegd aan index.html

--- Website/js/admin/admin.js
+++ Website/js/admin/admin.js
@@ -3,9 +3,7 @@
 		var $page = window.location.hash.replace('#','');
 		if($page === '' || $page == 'main') {
 			$webSocket.send({switchDashboard: 'main'});
-		} else if(['adminfunctions'].indexOf($page) != -1) {
-			$webSocket.send({switchDashboard: $page});
-		} else if (['grafieken'].indexOf($page) != -1) {
+		} else if(['adminfunctions','grafieken'].indexOf($page) != -1) {
 			$webSocket.send({switchDashboard: $page});
 		}
 	});
---
+++ Website/js/admin/dashboard_grafieken.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Thomas on 7-1-2015.
+ */
+
+
+    $webSocket.send({action: 'makeChart'});
+    alert("Actie verstuurd")
+
