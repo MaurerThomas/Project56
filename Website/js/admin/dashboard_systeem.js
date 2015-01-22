1ad166dc47e3bc9a2f3bd79070e6c0965e089c06
0868307 <0868307@hr.nl>
2015-01-22 15:03:44 +0100

    clear logs textarea on clear

--- Website/js/admin/dashboard_systeem.js
+++ Website/js/admin/dashboard_systeem.js
@@ -6,12 +6,14 @@
  {   
 	console.log("DEZE DING");
 	$webSocket.send({action: 'clearLog'});
+	$("#logs").html() = "";
  });
  $('#savesettings').click(function()
  {
 	console.log("Save");
 	$webSocket.send({action: 'cronjob',minute: $('minute1').val() , hour: $('hour1').val(), alternate: $('checkbox1').checked, cdromland: $('checkbox2').checked});
  });
+ 
  
  (function() {
 	var $saving = false, $deleting = false;
