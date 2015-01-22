b9fc08273744c04abdbe41661cc2d5301a3d9dbc
Thomas Maurer <0876337@hr.nl>
2015-01-22 15:10:17 +0100

    Afbeeldingen worden nu wel geladen(Sorry voor de alert, werkt anders niet. Weet niet waarom)

--- Website/js/admin/dashboard_grafieken.js
+++ Website/js/admin/dashboard_grafieken.js
@@ -1,17 +1,19 @@
  var grafiekList = [
-    "/img/LineChartProcessorenCoolers.jpeg",
-    "/img/LineChartProcessoren.jpeg",
-    "/img/LineChartVoedingen.jpeg",
-    "/img/LineChartMoederborden.jpeg",
-    "/img/LineChartGeheugen.jpeg ",
-    "/img/LineChartGrafischeKaarten.jpeg",
-    "/img/LineChartBehuizingen.jpeg",
-    "/img/LineChartSchijven.jpeg",  ];
+    "/img/LineChartProcessorenCoolers.jpeg?",
+    "/img/LineChartProcessoren.jpeg?",
+    "/img/LineChartVoedingen.jpeg?",
+    "/img/LineChartMoederborden.jpeg?",
+    "/img/LineChartGeheugen.jpeg?",
+    "/img/LineChartGrafischeKaarten.jpeg?",
+    "/img/LineChartBehuizingen.jpeg?",
+    "/img/LineChartSchijven.jpeg?",  ];
 
 
 $('#grafiek').change(function () {
+    d = new Date();
     var select = $('#grafiek option:selected').text();
     $webSocket.send({action:'makeChart','makeChart': select});
     var val = parseInt($('#grafiek').val());
-    $('#image').attr("src",grafiekList[val]);
+    alert("Grafiek wordt geladen..");
+    $('#image').attr("src",grafiekList[val]+d.getTime());
 });
