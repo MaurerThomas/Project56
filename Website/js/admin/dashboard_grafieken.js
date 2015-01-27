37f052640736e41d9b5c2c984700efb31735e9b6
EE <malusluminis@hotmail.com>
2015-01-27 12:07:45 +0100

    Thomas z'n dashboard veranderingen die ie nogsteeds niet had gepusht

--- Website/js/admin/dashboard_grafieken.js
+++ Website/js/admin/dashboard_grafieken.js
@@ -1,21 +1,24 @@
- $(function(){
- var grafiekList = [
-    "/img/LineChartProcessoren.jpeg?",
-    "/img/LineChartVoedingen.jpeg?",
-    "/img/LineChartMoederborden.jpeg?",
-    "/img/LineChartGrafischeKaarten.jpeg?",
-    "/img/LineChartBehuizingen.jpeg?",
-    "/img/LineChartSchijven.jpeg?",  ];
+ $(function() {
+ 	var grafiekList = [
+ 		"/img/LineChartProcessoren.jpeg?",
+ 		"/img/LineChartVoedingen.jpeg?",
+ 		"/img/LineChartMoederborden.jpeg?",
+ 		"/img/LineChartGrafischeKaarten.jpeg?",
+ 		"/img/LineChartBehuizingen.jpeg?",
+ 		"/img/LineChartSchijven.jpeg?"
+ 	];
 
-$("#grafiek option").each(function() {
-    var select = $(this).text();
-  $webSocket.send({action:'makeChart','makeChart': select});
-});
+ 	$("#grafiek option").each(function() {
+ 		var select = $(this).text();
+ 		$webSocket.send({
+ 			action: 'makeChart',
+ 			'makeChart': select
+ 		});
+ 	});
 
-$('#grafiek').change(function () {
-     d = new Date();
-    var val = parseInt($('#grafiek').val());
-    $('#image').attr("src",grafiekList[val]+d.getTime());
-
+ 	$('#grafiek').change(function() {
+ 		var d = new Date();
+ 		var val = parseInt($('#grafiek').val());
+ 		$('#image').attr("src", grafiekList[val] + d.getTime());
+ 	});
  });
-  }) ();
